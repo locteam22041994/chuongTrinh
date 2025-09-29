125433e1a3d4cedad48229c2318a3cceaedf53d8
Duong Minh Phuc Loc <locduongminphuc1@gmail.com>
2025-09-29 11:39:02 +0700

    phiên bản main

--- src/component/Detailpost.js
+++ src/component/Detailpost.js
@@ -1,47 +1,8 @@
-<<<<<<< HEAD
-import { Button, Form, Input } from "antd";
-import { useEffect } from "react";
-function Detailpost(props) {
-  const { setChitiet, Chitiet, handleSave, handleAdd, mode } = props;
-  const [form] = Form.useForm();
-
-  //Đồng bộ dữ liệu vào form khi Chitiet thay đổi
-  // useEffect(() => {
-  //   if (Chitiet) {
-  //     form.setFieldsValue({
-  //       title: Chitiet.title,
-  //       body: Chitiet.body,
-  //     });
-  //   }
-  // }, [Chitiet]);
-
-  const onFinish_them = (e) => {
-    if (!e.title || !e.body) {
-      alert("Both title and body are required.");
-      return;
-    }
-    fetch("https://dummyjson.com/posts/add", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({
-        userId: e["useId"],
-        title: e["title"],
-        body: e["body"],
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setChitiet?.(data);
-        handleAdd(data);
-      });
-  };
-=======
 import { Button, Form, Input } from 'antd'
 import { useEffect } from 'react'
 function Detailpost(props) {
   const { setChitiet, Chitiet, handleSave, mode } = props
   const [form] = Form.useForm()
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
 
   // Đồng bộ dữ liệu vào form khi Chitiet thay đổi
   useEffect(() => {
@@ -67,16 +28,6 @@ function Detailpost(props) {
     })
       .then((res) => res.json())
       .then((data) => {
-<<<<<<< HEAD
-        setChitiet?.(data);
-        handleSave(data);
-      });
-  };
-
-  const showChitiet = () => {
-    //console.log(props);
-    if (mode === "view") {
-=======
         setChitiet?.(data)
         handleSave(data)
       })
@@ -85,87 +36,31 @@ function Detailpost(props) {
   const showChitiet = () => {
     //console.log(props);
     if (mode === 'view') {
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
       return (
         <>
           <p>{props.Chitiet.title}</p>
           <p>{props.Chitiet.body}</p>
         </>
-<<<<<<< HEAD
-      );
-    } else if (mode === "add") {
-=======
       )
     } else {
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
       return (
         <>
           <Form
             form={form}
-<<<<<<< HEAD
-            onFinish={onFinish_them}
-=======
             onFinish={onFinish_Dieuchinhform}
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
             initialValues={{
               title: Chitiet?.title,
               body: Chitiet?.body,
             }}
           >
-<<<<<<< HEAD
-            <Form.Item name="useId">
-              <Input
-                placeholder="input text"
-                size="large"
-                style={{ width: 304 }}
-              ></Input>
-            </Form.Item>
-            <Form.Item name="title">
-=======
             <Form.Item name='title'>
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
               <Input
                 placeholder='input text'
                 size='large'
                 style={{ width: 304 }}
               ></Input>
             </Form.Item>
-<<<<<<< HEAD
-            <Form.Item name="body">
-              <Input
-                placeholder="input text"
-                size="large"
-                style={{ width: 304 }}
-              ></Input>
-            </Form.Item>
-            <Form.Item name="nut_bam_them">
-              <Button htmlType="submit">Thêm</Button>
-            </Form.Item>
-          </Form>
-        </>
-      );
-    } else {
-      return (
-        <>
-          <Form
-            form={form}
-            onFinish={onFinish_Dieuchinhform}
-            initialValues={{
-              title: Chitiet?.title,
-              body: Chitiet?.body,
-            }}
-          >
-            <Form.Item name="title">
-              <Input
-                placeholder="input text"
-                size="large"
-                style={{ width: 304 }}
-              ></Input>
-            </Form.Item>
-            <Form.Item name="body">
-=======
             <Form.Item name='body'>
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
               <Input
                 placeholder='input text'
                 size='large'
--- src/component/Post.js
+++ src/component/Post.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Button, Flex, Form, Input, Modal, Pagination, Table } from "antd";
 import { useEffect, useState } from "react";
 import Detailpost from "./Detailpost";
@@ -11,32 +10,17 @@ function Post() {
   const [Chitiet, setChitiet] = useState({});
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [mode, setMode] = useState("view"); // "view" | "edit"
-=======
-import { Button, Flex, Form, Input, Modal, Pagination, Table } from 'antd'
-import { useEffect, useState } from 'react'
-import Detailpost from './Detailpost'
-
-function Post() {
-  const [Post, setPost] = useState([])
-  const [Filter, setFilter] = useState({ limit: 10, skip: 0 })
-  const [Total, setTotal] = useState(0)
-  const [Query, setQuery] = useState('')
-  const [Chitiet, setChitiet] = useState({})
-  const [isOpenModal, setIsOpenModal] = useState(false)
-  const [mode, setMode] = useState('view') // "view" | "edit"
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
 
   useEffect(() => {
     fetch(
-      'https://dummyjson.com/posts/search?limit=' +
+      "https://dummyjson.com/posts/search?limit=" +
         Filter.limit +
-        '&skip=' +
+        "&skip=" +
         Filter.skip +
         Query
     )
       .then((res) => res.json())
       .then((data) => {
-<<<<<<< HEAD
         setPost(data.posts);
         setTotal(data.total);
       });
@@ -59,45 +43,14 @@ function Post() {
     setIsOpenModal(true);
   };
 
-  const clickThem = (id) => {
-    fetch("https://dummyjson.com/posts/" + id)
-      .then((res) => res.json())
-      .then((data) => {
-        setChitiet(data);
-        setMode("add");
-        setIsOpenModal(true);
-      });
-  };
-//chuẩn bị
   const handleSave = (data) => {
-    //let flag = false;
-    //console.log(Chitiet);
-    //console.log(data);
     const list = Post.map((current_post) => {
       if (current_post.id === data.id) {
-        //flag = true;
         return data;
       }
       return current_post;
     });
-    //if (flag === true) {
-    //console.log("save", list);
     setPost(list);
-    // } else {
-    //   let arr = [];
-    //   arr = [data, ...list];
-    //   setPost(arr);
-    // }
-
-    setIsOpenModal(false);
-  };
-
-  const handleAdd = (data) => {
-    console.log(data);
-    let arr = [];
-    arr = [data, ...Post];
-    //console.log("add", arr);
-    setPost(arr);
     setIsOpenModal(false);
   };
 
@@ -110,93 +63,37 @@ function Post() {
       key: "thaotac",
       render: (_, current_post) => (
         <Button onClick={() => clickChitiet(current_post.id, "view")}>
-=======
-        setPost(data.posts)
-        setTotal(data.total)
-      })
-  }, [Query, Filter])
-
-  const clickDieuchinh = (id) => {
-    fetch('https://dummyjson.com/posts/' + id)
-      .then((res) => res.json())
-      .then((data) => {
-        setChitiet(data)
-        setMode('edit')
-        setIsOpenModal(true)
-      })
-  }
-
-  const clickChitiet = (id, mode) => {
-    const current_post = Post.find((post) => post.id === id)
-    setChitiet(current_post)
-    setMode(mode)
-    setIsOpenModal(true)
-  }
-
-  const handleSave = (data) => {
-    const list = Post.map((current_post) => {
-      if (current_post.id === data.id) {
-        return data
-      }
-      return current_post
-    })
-    setPost(list)
-    setIsOpenModal(false)
-  }
-
-  const columns = [
-    { title: 'Title', dataIndex: 'title', key: 'title' },
-    { title: 'Body', dataIndex: 'body', key: 'body' },
-    {
-      title: 'Thao tác',
-      dataIndex: 'thaotac',
-      key: 'thaotac',
-      render: (_, current_post) => (
-        <Button onClick={() => clickChitiet(current_post.id, 'view')}>
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
           Chi tiết
         </Button>
       ),
     },
     {
-<<<<<<< HEAD
       title: "Điều chỉnh",
       dataIndex: "thaotacdieuchinh",
       key: "thaotacdieuchinh",
       render: (_, current_post) => (
         <Button onClick={() => clickChitiet(current_post.id, "edit")}>
-=======
-      title: 'Điều chỉnh',
-      dataIndex: 'thaotacdieuchinh',
-      key: 'thaotacdieuchinh',
-      render: (_, current_post) => (
-        <Button onClick={() => clickChitiet(current_post.id, 'edit')}>
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
           Điều chỉnh
         </Button>
       ),
     },
-<<<<<<< HEAD
   ];
-=======
-  ]
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
 
   const onFinish_form = (e) => {
-    setQuery('&q=' + Number(e['tim_kiem']))
-    setFilter({ limit: Filter.limit, skip: 0 })
-  }
+    setQuery("&q=" + Number(e["tim_kiem"]));
+    setFilter({ limit: Filter.limit, skip: 0 });
+  };
 
   const Changpage = (page, pageSize) => {
-    setFilter({ limit: pageSize, skip: Filter.limit * (page - 1) })
-  }
+    setFilter({ limit: pageSize, skip: Filter.limit * (page - 1) });
+  };
 
   const ItemPage = (current, pageSize) => {
-<<<<<<< HEAD
     setFilter({ limit: pageSize, skip: Filter.limit * (current - 1) });
   };
 
   const Hientotal = () => <div>{Total}</div>;
+  console.log(Post);
 
   return (
     <div className="block">
@@ -214,34 +111,7 @@ function Post() {
           </Form.Item>
         </Flex>
       </Form>
-      <Flex gap="5px">
-        <Button onClick={() => clickThem()}>Thêm</Button>
-      </Flex>
-=======
-    setFilter({ limit: pageSize, skip: Filter.limit * (current - 1) })
-  }
 
-  const Hientotal = () => <div>{Total}</div>
-  console.log(Post)
-
-  return (
-    <div className='block'>
-      <Form onFinish={onFinish_form}>
-        <Flex gap='5px'>
-          <Form.Item name='tim_kiem'>
-            <Input
-              placeholder='input search text'
-              size='large'
-              style={{ width: 304 }}
-            />
-          </Form.Item>
-          <Form.Item name='nut_bam'>
-            <Button htmlType='submit'>Tìm kiếm</Button>
-          </Form.Item>
-        </Flex>
-      </Form>
-
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
       <Table dataSource={Post} columns={columns} />
 
       <Pagination
@@ -250,11 +120,7 @@ function Post() {
         onShowSizeChange={ItemPage}
         showSizeChanger
         total={Total}
-<<<<<<< HEAD
         style={{ paddingLeft: "20px" }}
-=======
-        style={{ paddingLeft: '20px' }}
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
         showTotal={Hientotal}
         onChange={Changpage}
       />
@@ -270,8 +136,6 @@ function Post() {
           Chitiet={Chitiet}
           setChitiet={setChitiet}
           handleSave={handleSave}
-<<<<<<< HEAD
-          handleAdd={handleAdd}
         />
       </Modal>
     </div>
@@ -279,12 +143,3 @@ function Post() {
 }
 
 export default Post;
-=======
-        />
-      </Modal>
-    </div>
-  )
-}
-
-export default Post
->>>>>>> a7d945bc2450da4fea87b1d8dac89139f07ed3e0
