084b7bc54ee36ba9eb0d28e09c4c032f33871bd0
Duong Minh Phuc Loc <locduongminphuc1@gmail.com>
2025-09-30 10:34:36 +0700

    mới nhất login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React, { StrictMode, useState } from "react";
+import { NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Post from "./component/Post";
 import MenuAntd from "./component/MenuAntd";
+import Login from "./component/Login";
 function App() {
   return (
     <>
       <div className="flex gap-5">
-        <MenuAntd />
+        <Routes>
+          <Route path="/" element={<MenuAntd />} />
+          <Route path="/post/list" element={<Post />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        {/* <Post /> */}
       </div>
     </>
   );
